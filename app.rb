dcbb19e65fe19f0f74b7bc73db65c00adda2fd1c
Kevin Marsh <kevin.marsh@gmail.com>
2022-01-07 11:53:12 -0500

    Adds parts of old app

--- app.rb
+++ app.rb
@@ -7,10 +7,23 @@ require "sequel"
 DB = Sequel.connect(ENV["DATABASE_URL"] || "sqlite:///data/voters.db")
 VOTERS = DB[:voters]
 
-get "/" do
-  "<h1>#{VOTERS.count} Ohio Voters</h1>"
+helpers do
+  def age(dob)
+    dob = Date.parse(dob) unless dob.is_a?(Date)
+
+    now = Time.now.utc.to_date
+    now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+  end
 end
 
-get "/:name" do
-  "<h1>Hello From Ruby on Fly!</h1></br>and hello to #{params[:name]}"
+get "/" do
+  if (last_name = params[:last_name].to_s.strip) != ""
+    @voters = VOTERS.where(Sequel.ilike(:LAST_NAME, "%#{last_name}%"))
+                    .order(:first_name)
+  elsif (address = params[:address].to_s.strip) != ""
+    @voters = VOTERS.where(Sequel.ilike(:RESIDENTIAL_ADDRESS1, "%#{address}%"))
+                    .order(:residential_address1)
+  end
+
+  erb :'index'
 end
